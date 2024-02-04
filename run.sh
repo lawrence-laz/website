bc7fb879b81cef073b889d7a9553490121b884c3
Laurynas Lazauskas <8823448+lawrence-laz@users.noreply.github.com>
2024-02-04 19:50:32 +0200

    Add VeganLT app landing page

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-bundle exec jekyll serve --livereload && xdg-open http://localhost:4000
+open -na "Google Chrome" --args --new-window "http://localhost:4000" && bundle exec jekyll serve --livereload 
+# xdg-open http://localhost:4000
